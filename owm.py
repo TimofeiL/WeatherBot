149b1c6846abae47b58a0f8ba2b74daa781be4c0
TimofeiL <dreamtimichl@gmail.com>
2023-02-03 16:29:16 +0200

    Weather Telegram bot

---
+++ owm.py
@@ -0,0 +1,26 @@
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+
+import telebot
+
+config_dict = config.get_default_config()
+config_dict['language'] = 'ru'
+owm = OWM('854ad977e3ff1041d70dd0e5b1bad5b7', config_dict)
+mgr = owm.weather_manager()
+
+bot = telebot.TeleBot("1853857581:AAHlSj1TNlhrCTTqSxQ4d0aWO-FPDXi5SlM", parse_mode=None)
+@bot.message_handler(content_types=['text'])
+
+def send_welcome(message):
+	place = message.text
+	try:
+		observation = mgr.weather_at_place(place)
+		w = observation.weather
+		bot.send_message(message.chat.id, 'В городе/стране ' + place + ' сейчас ' + str(w.temperature('celsius')['temp']) + ' градуса(ов) по цельсию')
+		bot.send_message(message.chat.id, 'Ветер ' + str(w.wind()['speed']) + ' метров в секунду')
+		bot.send_message(message.chat.id, 'На улице сейчас ' + w.detailed_status)
+	except:
+		bot.send_message(message.chat.id, 'Данные введены некорректно!')
+
+bot.polling(none_stop = True)
